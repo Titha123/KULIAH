f2ea0b489f9396aa5e819105b633a4e59f3ca8b0
Destitha Almaqvira Bachmid <bachmiddestitha@gmail.com>
2025-08-20 18:02:13 +0700

    test commit

---
+++ DART/project_satu/bin/project_satu.dart
@@ -0,0 +1,5 @@
+import 'package:project_satu/project_satu.dart' as project_satu;
+
+void main(List<String> arguments) {
+  print('Hello ${project_satu.calculate()}!');
+}
---
+++ DART/project_satu/test/project_satu_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_satu/project_satu.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
