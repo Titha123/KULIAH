88b725ea42ed36a27d91f2abae6db59701f58737
Destitha Almaqvira Bachmid <bachmiddestitha@gmail.com>
2025-08-26 09:38:48 +0700

    variabel dan tipe data

---
+++ DART/project_satu/bin/latihansatu.dart
@@ -0,0 +1,19 @@
+void main (){
+  String nama = "Destitha Almaqvira Bachmid";
+  int umur = 20;
+  print("Nama saya adalah $nama dan umur saya $umur tahun");
+
+  double tinggi = 155.5;
+  print("Tinggi saya adalah $tinggi cm");
+
+  dynamic nilai = "A";
+  print("Nilai mobile semester ini adalah $nilai");
+
+  num gaji = 5000000;
+  print("Gaji saya adalah $gaji perhari");
+
+  bool gajian = false;
+  print("Apakah saya benar sudah digaji? $gajian");
+
+  print ("apakah nilai mobile semester ini adalah A? ${nilai == "A"}");
+}
